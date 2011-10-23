05ebf9935049511cf523f4d491dbad2d7bd61d18
Viktor Kucsera <viktor.kucsera@gmail.com>
2011-10-24 01:12:50 +0200

    added output "Hello world!"

--- ICP-IS-MHD/src/sk/fiit/kucsera_kovalcik/icp/ismhd/HelloWorld.java
+++ ICP-IS-MHD/src/sk/fiit/kucsera_kovalcik/icp/ismhd/HelloWorld.java
@@ -13,8 +13,7 @@ public final class HelloWorld {
      * @param args
      */
     public static void main(String[] args) {
-        // TODO Auto-generated method stub
-
+        System.out.println("Hello world!");
     }
 
 }
