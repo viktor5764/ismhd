86d693f20d7e7142f687f5a536d6910cb4e6939c
Viktor Kucsera <viktor.kucsera@gmail.com>
2011-10-24 01:06:27 +0200

    created class HelloWorld

---
+++ ICP-IS-MHD/src/sk/fiit/kucsera_kovalcik/icp/ismhd/HelloWorld.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package sk.fiit.kucsera_kovalcik.icp.ismhd;
+
+/**
+ * @author Viktor Kucsera
+ *
+ */
+public final class HelloWorld {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
